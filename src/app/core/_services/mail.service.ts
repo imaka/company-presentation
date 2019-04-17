c9811113f3a368ecc36a2f634947bc1ec0096d5f
Diego <d.perezmartinez@gmail.com>
2019-04-17 15:16:56 +0200

    Add mail service

---
+++ src/app/core/_services/mail.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '@environments/environment.prod';
+import { tap, catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MailService {
+  constructor(private http: HttpClient) {}
+
+  send(subject: string, text: string) {
+    const body = {
+      subject: subject,
+      text: text
+    };
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    this.http.post(environment.gcp_send_mail, body, httpOptions).pipe(
+      tap(_ => console.log(`sending email: ${_}`)),
+      catchError(this.handleError('send mail', []))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
