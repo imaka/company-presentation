2a16b8900995ccba83c9fc6f7066c32a6e1a40f0
Diego <d.perezmartinez@gmail.com>
2020-10-22 15:15:32 +0100

    General updates

--- prettier.config.js
+++ prettier.config.js
@@ -4,8 +4,8 @@ module.exports = {
   useTabs: false,
   semi: true,
   singleQuote: true,
-  trailingComma: "none", // other options `es5` or `all`
+  trailingComma: 'none', // other options `es5` or `all`
   bracketSpacing: true,
-  arrowParens: "avoid", // other option 'always'
-  parser: "typescript"
+  arrowParens: 'avoid' // other option 'always'
+  //parser: "typescript"
 };
