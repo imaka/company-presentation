4a94a672c4f4c8e48342ec4db9f9efb5f1a1c0b6
Diego <d.perezmartinez@gmail.com>
2020-10-28 18:20:23 +0000

    Adapt coming soon page

--- src/app/coming-soon/coming-soon/coming-soon.component.ts
+++ src/app/coming-soon/coming-soon/coming-soon.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-coming-soon',
   templateUrl: './coming-soon.component.html',
-  styleUrls: ['./coming-soon.component.css']
+  styleUrls: ['./coming-soon.component.scss']
 })
 export class ComingSoonComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
