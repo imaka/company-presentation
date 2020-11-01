c1929d8bda01950530cb59e402e3c20c8f289b06
Diego <d.perezmartinez@gmail.com>
2020-11-01 16:17:38 +0000

    Fix on http-interceptor

--- src/app/core/_interceptors/http-error.interceptor.ts
+++ src/app/core/_interceptors/http-error.interceptor.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError, EMPTY } from 'rxjs';
 import { Router } from '@angular/router';
-import { catchError, finalize } from 'rxjs/operators';
+import { catchError, finalize, retry } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class HttpErrorInterceptor implements HttpInterceptor {
   constructor(private router: Router) {}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
-      catchError(response => {
-        return throwError(response);
-      }),
-      finalize(() => {
-        this.router.navigateByUrl('/coming-soon');
-        return EMPTY;
+      retry(1),
+      catchError((response: HttpErrorResponse) => {
+        if (response.error instanceof ErrorEvent) {
+          // client-side error
+          return throwError(response.error.message);
+        } else {
+          // server-side error
+          this.router.navigateByUrl('/coming-soon');
+          return EMPTY;
+        }
       })
     );
   }
